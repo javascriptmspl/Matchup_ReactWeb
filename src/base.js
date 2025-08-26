f78914023d2b287cc1072415560dfbf40000c2cf
devloper <devloper.mspl@gmail.com>
2025-08-26 16:57:36 +0530

    l....

--- src/base.js
+++ src/base.js
@@ -2,4 +2,4 @@ import { UserData } from "./assets/DummyData/userData";
 import { LOCAL_USER_GENDER } from "./utils";
 
 // export const BASE_URL = "https://datingapi.meander.software";
-export const BASE_URL = "http://173.249.48.121:4457";
+export const BASE_URL = "http://173.249.48.121:4457"; 
