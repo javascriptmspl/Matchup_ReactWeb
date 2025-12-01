4b091c29c93af77a8913257a5d26c58d3181dff2
developer.mspl <developer.mspl@gmail.com>
2025-12-01 13:45:34 +0530

    add multi language

--- src/dating/component/popUps/LanguageChange.jsx
+++ src/dating/component/popUps/LanguageChange.jsx
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
 import { Modal } from 'react-bootstrap';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
 
 const LanguageChange = ({ showModal, hideModal }) => {
-  const languages = ['English', 'Spanish', 'French', 'German', 'Chinese', 'Japanese', 'Korean', 'Russian', 'Arabic'];
-  const [selectedLanguage, setSelectedLanguage] = useState(null);
+
+  const languages = [
+    { code: "en", name: "English" },
+    { code: "hi", name: "Hindi" },
+    { code: "gu", name: "Gujarati" },
+    { code: "mr", name: "Marathi" },
+    { code: "pa", name: "Punjabi" },
+    { code: "te", name: "Telugu" },
+    { code: "fr", name: "French" },
+    { code: "ja", name: "Japanese" },
+    { code: "es", name: "Spanish" },
+    { code: "ar", name: "Arabic" },
+    { code: "ko", name: "Korean" },
+    { code: "de", name: "German" },
+    { code: "ru", name: "Russian" },
+  ];
+
+  const [selectedLanguage, setSelectedLanguage] = useState("English");
 
   const handleLanguageSelect = (language) => {
-    setSelectedLanguage(language);
+    setSelectedLanguage(language.name);
+
+    const googleCombo = document.querySelector(".goog-te-combo");
+    if (googleCombo) {
+      googleCombo.value = language.code;
+      googleCombo.dispatchEvent(new Event("change"));
+    }
   };
 
   return (
@@ -18,32 +38,35 @@ const LanguageChange = ({ showModal, hideModal }) => {
           className="fa fa-times fs-3"
           aria-hidden="true"
           style={{
-            cursor: 'pointer',
-            float: 'right',
-            padding: '15px 25px 0 0',
+            cursor: "pointer",
+            float: "right",
+            padding: "15px 25px 0 0",
           }}
         ></i>
       </span>
+
       <div className="container px-5 pb-5">
         <h5 className='text-center mb-4'>Select Language</h5>
+
         <div className="row">
           {languages.map((language, index) => (
             <div key={index} className="col-6">
               <p
-                className={`rounded-4 top-quiz-option pointer py-3 ps-4 ${selectedLanguage === language ? 'selected-option' : ''}`}
+                className={`rounded-4 top-quiz-option pointer py-3 ps-4 ${selectedLanguage === language.name ? "selected-option" : ""}`}
                 onClick={() => handleLanguageSelect(language)}
                 style={{
-                  backgroundColor: selectedLanguage === language ? '#f24570' : 'whitesmoke',
-                  color: selectedLanguage === language ? 'white' : 'black',
+                  backgroundColor: selectedLanguage === language.name ? "#f24570" : "whitesmoke",
+                  color: selectedLanguage === language.name ? "white" : "black",
                 }}
               >
-               <i class="fa fa-language me-3 " aria-hidden="true"></i>
-  {language}
+                <i className="fa fa-language me-3" aria-hidden="true"></i>
+                {language.name}
               </p>
             </div>
           ))}
         </div>
-        <button className="default-btn reverse mt-4 " style={{ float: 'right' }} onClick={hideModal}>
+
+        <button className="default-btn reverse mt-4" style={{ float: "right" }} onClick={hideModal}>
           <span>Save</span>
         </button>
       </div>
